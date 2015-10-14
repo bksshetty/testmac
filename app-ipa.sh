a5338fe51c44c16eb2945ae130239596a2d6c784
Kirthan Shetty <kirthan.b@openly.co>
2015-10-14 23:57:05 +0530

    ipa file

--- app-ipa.sh
+++ app-ipa.sh
@@ -2,8 +2,8 @@
 #IPA_NAME=`find . -iname *.app`
 #CRASHLYTICS=`find . -iname 'Crashlytics.framework'`
 mkdir Payload
-find . -name 'AW16.app' -exec mv {} Payload  \;
+find . -name 'AW16.app' -exec mv {} Payload/  \;
 echo "copied"
-ls Payload
 zip -r aw16app.zip Payload
 mv aw16app.zip aw16app.ipa
+ls Payload
