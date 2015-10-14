9f35c4f123a456f80b23d56192f17335b76c430e
Kirthan Shetty <kirthan.b@openly.co>
2015-10-15 00:32:18 +0530

    ipa file

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-IPA_NAME=`find . -iname *.ipa`
-CRASHLYTICS=`find . -iname 'Crashlytics.framework'`
-if [ -f "$IPA_NAME" ] && [ -d "$CRASHLYTICS" ]; then
-"$CRASHLYTICS/submit" $API_KEY $BUILD_SECRET -ipaPath "$IPA_NAME" -emails kirthanshetty@hotmail.com
-else
-echo "Error deploying ipa to crashlytics: no ipa or crashlytics framework found!";
-exit 1
-fi
