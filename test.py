a08b13ba7aeb57618db9600ac4fce134661424a6
Kirthan Shetty <kirthan.b@openly.co>
2015-10-16 20:05:01 +0530

    added ipa test

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
-import unittest
-from os.path import abspath
+from random import randint
 from appium import webdriver
-from time import sleep
-from appium.webdriver.common.touch_action import TouchAction
+from appium import SauceTestCase, on_platforms
 
+
+platforms = [{
+                'platformName': 'iOS',
+                'platformVersion': '7.1',
+                'deviceName': 'iPhone Simulator',
+                'app': 'http://appium.s3.amazonaws.com/TestApp6.0.app.zip',
+                'appiumVersion': '1.3.4'
+            }]
+
+@on_platforms(platforms)
 class  AppiumTests(unittest.TestCase):
-
-    def setUp(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'iOS'
-        desired_caps['platformVersion'] = '8.4'
-        desired_caps['deviceName'] = 'iPad Air'
-        desired_caps['app'] = abspath('/Users/openly/Desktop/AW16.app')
-        desired_caps['appiumVersion'] = '1.4.8'
-        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-        
-
-    def tearDown(self):
-        self.driver.quit()
 
     def testCases(self):
         self.driver.implicitly_wait(60)
@@ -28,16 +23,10 @@ class  AppiumTests(unittest.TestCase):
           password = self.driver.find_element_by_name('pass')
           password.set_value('test')
           self.driver.find_element_by_name('LOGIN').click()
-          self.driver.find_element_by_name('GLOBAL').click()
-          self.driver.find_element_by_name('APPLY').click()
-          self.driver.find_element_by_name('CONTINUE').click()
-          self.driver.find_element_by_name('MENU').click()
+          # self.driver.find_element_by_name('GLOBAL').click()
+          # self.driver.find_element_by_name('APPLY').click()
+          # self.driver.find_element_by_name('CONTINUE').click()
+          # self.driver.find_element_by_name('MENU').click()
            # action.tap(el).perform()
         except Exception, e:
             print e
-        
-        
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(AppiumTests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ test1.py
@@ -0,0 +1,30 @@
+"""An example of Appium running on Sauce.
+
+This test assumes SAUCE_USERNAME and SAUCE_ACCESS_KEY are environment variables
+set to your Sauce Labs username and access key."""
+
+from random import randint
+from appium import webdriver
+from appium import SauceTestCase, on_platforms
+
+
+platforms = [{
+                'platformName': 'iOS',
+                'platformVersion': '7.1',
+                'deviceName': 'iPhone Simulator',
+                'app': 'http://appium.s3.amazonaws.com/TestApp6.0.app.zip',
+                'appiumVersion': '1.3.4'
+            }]
+
+@on_platforms(platforms)
+class SimpleIOSSauceTests(SauceTestCase):
+
+    def _populate(self):
+        # populate text fields with two random numbers
+        els = self.driver.find_elements_by_class_name('UIATextField')
+
+        self._sum = 0
+        for i in range(2):
+            rnd = randint(0, 10)
+            els[i].send_keys(rnd)
+            self._sum += rnd
