44f2b9ad93a5e60451dc48fae971ebb9682abbbe
Kirthan Shetty <kirthan.b@openly.co>
2015-10-20 16:53:22 +0530

    added new folders

--- test1.py
+++ test1.py
@@ -1,5 +1,4 @@
 """An example of Appium running on Sauce.
-
 This test assumes SAUCE_USERNAME and SAUCE_ACCESS_KEY are environment variables
 set to your Sauce Labs username and access key."""
 
@@ -9,22 +8,28 @@ from appium import SauceTestCase, on_platforms
 
 
 platforms = [{
-                'platformName': 'iOS',
-                'platformVersion': '7.1',
-                'deviceName': 'iPhone Simulator',
-                'app': 'http://appium.s3.amazonaws.com/TestApp6.0.app.zip',
-                'appiumVersion': '1.3.4'
+                caps['browserName'] = ""
+                caps['appiumVersion'] = "1.4.13"
+                caps['deviceName'] = "iPad Simulator"
+                caps['platformVersion'] = "8.4"
+                caps['platformName'] = "iOS"
+                caps['app'] = "sauce-storage:AW16.ipa"
             }]
 
 @on_platforms(platforms)
 class SimpleIOSSauceTests(SauceTestCase):
 
-    def _populate(self):
-        # populate text fields with two random numbers
-        els = self.driver.find_elements_by_class_name('UIATextField')
-
-        self._sum = 0
-        for i in range(2):
-            rnd = randint(0, 10)
-            els[i].send_keys(rnd)
-            self._sum += rnd
+    def testCases(self):
+        self.driver.implicitly_wait(60)
+        try:
+          #  action = TouchAction(self.driver)
+          email = self.driver.find_element_by_name('email')
+          email.set_value('demo')
+          password = self.driver.find_element_by_name('pass')
+          password.set_value('test')
+          self.driver.find_element_by_name('LOGIN').click()
+          self.driver.find_element_by_name('GLOBAL').click()
+          self.driver.find_element_by_name('APPLY').click()
+           # action.tap(el).perform()
+        except Exception, e:
+            print e
