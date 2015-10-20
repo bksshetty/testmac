ca821e51975420bac8898c16bddfb1b6065a8a43
Kirthan Shetty <kirthan.b@openly.co>
2015-10-20 19:25:05 +0530

    added new folders

--- test1.py
+++ test1.py
@@ -1,23 +1,23 @@
-"""An example of Appium running on Sauce.
-This test assumes SAUCE_USERNAME and SAUCE_ACCESS_KEY are environment variables
-set to your Sauce Labs username and access key."""
-
-from random import randint
+import unittest
+from os.path import abspath
 from appium import webdriver
-from appium import SauceTestCase, on_platforms
+from time import sleep
+from appium.webdriver.common.touch_action import TouchAction
 
+class  AppiumTests(unittest.TestCase):
 
-platforms = [{
-                caps['browserName'] = ""
-                caps['appiumVersion'] = "1.4.13"
-                caps['deviceName'] = "iPad Simulator"
-                caps['platformVersion'] = "8.4"
-                caps['platformName'] = "iOS"
-                caps['app'] = "sauce-storage:AW16.ipa"
-            }]
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'iOS'
+        desired_caps['platformVersion'] = '8.4'
+        desired_caps['deviceName'] = 'iPad Air'
+        desired_caps['app'] = abspath('/Users/distiller/testmac/AW16.ipa')
+        desired_caps['appiumVersion'] = '1.4.8'
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+        
 
-@on_platforms(platforms)
-class SimpleIOSSauceTests(SauceTestCase):
+    def tearDown(self):
+        self.driver.quit()
 
     def testCases(self):
         self.driver.implicitly_wait(60)
@@ -28,6 +28,16 @@ class SimpleIOSSauceTests(SauceTestCase):
           password = self.driver.find_element_by_name('pass')
           password.set_value('test')
           self.driver.find_element_by_name('LOGIN').click()
+          self.driver.find_element_by_name('GLOBAL').click()
+          self.driver.find_element_by_name('APPLY').click()
+          self.driver.find_element_by_name('CONTINUE').click()
+          self.driver.find_element_by_name('MENU').click()
            # action.tap(el).perform()
         except Exception, e:
             print e
+        
+        
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(AppiumTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
