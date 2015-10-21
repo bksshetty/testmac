952b8c59a0c8c81f95ab7f79e2bbea203df0a53f
Kirthan Shetty <kirthan.b@openly.co>
2015-10-21 17:00:17 +0530

    new files

--- test1.py
+++ test1.py
@@ -11,7 +11,7 @@ class  AppiumTests(unittest.TestCase):
         desired_caps['platformName'] = 'iOS'
         desired_caps['platformVersion'] = '8.4'
         desired_caps['deviceName'] = 'iPad Air'
-        desired_caps['app'] = abspath('/Users/distiller/testmac/AW16.ipa')
+        desired_caps['app'] = abspath('/Users/distiller/Library/Developer/Xcode/DerivedData/ClarksCollection-alxmlvsmhisrlbcdwhucuoexpvtb/Build/Products/Debug-iphonesimulator/AW16.app')
         desired_caps['appiumVersion'] = '1.4.8'
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
         
